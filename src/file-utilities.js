db4c3cf3e7a02d65417e46f09928f62500000053
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2017-09-23 17:01:01 -0400

    Navigating to test from multiple sources

--- src/file-utilities.js
+++ src/file-utilities.js
@@ -62,18 +62,32 @@ let fileUtilities = {
     // component.hbs or component.js -> integration-test.js
     if ((namespace === 'templates' && optNamespace === 'components') || namespace === 'components') {
       let modifiedApplicationPath = applicationPath.replace('addon', 'tests').replace('app', 'tests');
-      let destination = path.join(modifiedApplicationPath, 'integration', 'components', optExtraFolders, `${fileName}-test.js`);
-      fileUtilities._openFile(destination);
+      let integrationTest = path.join(modifiedApplicationPath, 'integration', 'components', optExtraFolders, `${fileName}-test.js`);
+      fileUtilities._openFile(integrationTest);
     }
 
     // integration-test.js -> component.js
     else if (location === 'tests' && namespace === 'integration' && optNamespace === 'components') {
       let modifiedFileName = fileName.replace('-test', '');
-      let destination1 = path.join(root, 'addon', 'components', optExtraFolders, `${modifiedFileName}.js`);
-      let destination2 = path.join(root, 'app', 'components', optExtraFolders, `${modifiedFileName}.js`);
+      let addonPath = path.join(root, 'addon', 'components', optExtraFolders, `${modifiedFileName}.js`);
 
-      fileUtilities._openFile(destination1); // addon
-      fileUtilities._openFile(destination2); // app
+      if (fs.existsSync(addonPath)) {
+        fileUtilities._openFile(addonPath); // addon
+      } else {
+        let appPath = path.join(root, 'app', 'components', optExtraFolders, `${modifiedFileName}.js`);
+        fileUtilities._openFile(appPath); // app
+      }
+    }
+
+    // model.js -> unit-test.js
+    // controller.js -> unit-test.js
+    // route.js -> unit-test.js
+    // service.js -> unit-test.js
+    // helper.js -> unit-test.js
+    else if (['models', 'controllers', 'routes', 'services', 'helpers'].includes(namespace)) {
+      let modifiedApplicationPath = applicationPath.replace('addon', 'tests').replace('app', 'tests');
+      let unitTest = path.join(modifiedApplicationPath, 'unit', namespace, optExtraFolders, `${fileName}-test.js`);
+      fileUtilities._openFile(unitTest);
     }
   },
 
@@ -87,23 +101,27 @@ let fileUtilities = {
 
     // component.hbs -> component.js
     if (namespace === 'templates' && optNamespace === 'components' && extension === 'hbs') {
-      fileUtilities._openFile(path.join(applicationPath, 'components', optExtraFolders, `${fileName}.js`));
+      let component = path.join(applicationPath, 'components', optExtraFolders, `${fileName}.js`);
+      fileUtilities._openFile(component);
     }
 
     // component.js -> component.hbs
     else if (namespace === 'components' && extension === 'js') {
-      fileUtilities._openFile(path.join(applicationPath, 'templates', 'components', optExtraFolders, `${fileName}.hbs`));
+      let template = path.join(applicationPath, 'templates', 'components', optExtraFolders, `${fileName}.hbs`);
+      fileUtilities._openFile(template);
     }
 
     // route.js -> template.hbs
     // controller.js -> template.hbs
     else if ((namespace === 'controllers' || namespace === 'routes') && extension === 'js') {
-      fileUtilities._openFile(path.join(applicationPath, 'templates', optExtraFolders, `${fileName}.hbs`));
+      let template = path.join(applicationPath, 'templates', optExtraFolders, `${fileName}.hbs`);
+      fileUtilities._openFile(template);
     }
 
     // template.hbs -> controller.js
     else if (namespace === 'templates' && extension === 'hbs') {
-      fileUtilities._openFile(path.join(applicationPath, 'controllers', optExtraFolders, `${fileName}.js`));
+      let controller = path.join(applicationPath, 'controllers', optExtraFolders, `${fileName}.js`);
+      fileUtilities._openFile(controller);
     }
   },
 
@@ -116,12 +134,14 @@ let fileUtilities = {
     // controller.js -> route.js
     // template.hbs -> route.js
     if ((namespace === 'controllers' && extension === 'js') || (namespace === 'templates' && extension === 'hbs')) {
-      fileUtilities._openFile(path.join(applicationPath, 'routes', optExtraFolders, `${fileName}.js`));
+      let route = path.join(applicationPath, 'routes', optExtraFolders, `${fileName}.js`);
+      fileUtilities._openFile(route);
     }
 
     // route.js -> controller.js
     else if (namespace === 'routes' && extension === 'js') {
-      fileUtilities._openFile(path.join(applicationPath, 'controllers', optExtraFolders, `${fileName}.js`));
+      let controller = path.join(applicationPath, 'controllers', optExtraFolders, `${fileName}.js`);
+      fileUtilities._openFile(controller);
     }
   }
 
