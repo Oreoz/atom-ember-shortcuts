b103ef99c2937f4c9c8018926af3bd78e175d30e
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2017-10-12 07:09:48 -0400

    First draft of contextual component crawling.

--- src/helpers.js
+++ src/helpers.js
@@ -19,6 +19,34 @@ export function getPossibleIntentions(textEditor, bufferPosition) {
   let componentMatches = /{{[#\/]?([a-z-\/]+)/.exec(activeEditorRow);
   let componentName = componentMatches ? componentMatches[1] : '';
 
+  let contextualMatches = /{{([a-z-]*)\.([a-z-]+)/.exec(activeEditorRow);
+  if (contextualMatches) {
+    let regex = new RegExp(`\\{\\{#.*\\|(${contextualMatches[1]})\\|`);
+    let occurences = textEditor.buffer.findAllSync(regex);
+
+    if (occurences.length > 0) {
+      let parentDeclaration = textEditor.buffer.lineForRow(occurences[0].start.row);
+      let parentMatches = /{{[#\/]?([a-z-\/]+)/.exec(parentDeclaration);
+      let parentComponentName = parentMatches ? parentMatches[1] : '';
+
+      let parentTemplate = path.join(applicationPath, 'templates', 'components', `${parentComponentName}.hbs`);
+
+      fs.readFile(parentTemplate, 'utf-8', (error, data) => {
+        if (data) {
+          let yieldedRegex = new RegExp(`${contextualMatches[2]}=\\(component\\s['"]([a-z-\/]+)['"]\\)`);
+          let yieldedMatches = yieldedRegex.exec(data);
+          let yieldedComponentName = yieldedMatches ? yieldedMatches[1] : '';
+
+          let hbs = path.join(applicationPath, 'templates', 'components', `${yieldedComponentName}.hbs`);
+          let js = path.join(applicationPath, 'components', `${yieldedComponentName}.hbs`);
+
+          console.log(hbs, js);
+        }
+      });
+    }
+  }
+
+
   if (componentName) {
     let cleanedComponentName = componentName.replace('/', '\\');
 
